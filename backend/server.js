3825de14af3d56244c9719ece676327806f5c1e6
SpaceSC <ceciliasandor.22@gmail.com>
2021-07-19 18:12:17 +0200

    Add start.js

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,9 @@
 const express = require('express');
 const cors = require("cors");
 const app = express();
-const dotenv = require("dotenv");
-const mongoose = require("mongoose");
-const connectDB = require("./config/db");
-const port = 5000;
+
 //Import routes
 const Route = require("./routes/Route");
-
-dotenv.config();
-
-connectDB();
 
 //Middlewares
 app.use(express.json());
@@ -19,6 +12,6 @@ app.use(express.json());
 app.use("/api", Route);
 app.use(cors());
 
-app.listen(port, () => {
-  console.log(`Express server is running on port ${port}`);
-});
+
+
+module.exports = app;
