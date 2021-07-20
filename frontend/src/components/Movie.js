8a38663b7d468ad42074746a361fcd7fe4fcfca4
evagurubi <evagurubi@gmail.com>
2021-07-20 20:36:08 +0200

    Does not map

---
+++ frontend/src/components/Movie.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Movie({ item }) {
+  return (
+    <div>
+      <p>Hello</p>
+    </div>
+  );
+}
+
+export default Movie;
--- frontend/src/components/MovieList.js
+++ frontend/src/components/MovieList.js
@@ -1,20 +1,38 @@
 import React from "react";
 import { useState, useEffect } from "react";
+import Movie from "./Movie";
 
 function MovieList() {
-  const fetchData = () => {
+  const [data, setData] = useState([]);
+
+  const fetchData = async () => {
     fetch(
       "https://api.themoviedb.org/3/discover/movie?primary_release_year=2010&sort_by=vote_average.desc&api_key=57cbfa5ccc0ed1de4877b2f8f5a36d30"
     )
-      .then((res) => res.json())
-      .then((json) => console.log(json));
+      .then((res) => {
+        if (res.status !== 200) return "It is still loading";
+
+        return res.json();
+      })
+      .then((json) => {
+        setData(json);
+        // console.log(json);
+      });
   };
 
   useEffect(() => {
     fetchData();
   }, []);
 
-  return <div></div>;
+  return (
+    <div>
+      <Movie />
+      {data &&
+        data.results.map((item, i) => {
+          <Movie item={item} key={i} />;
+        })}
+    </div>
+  );
 }
 
 export default MovieList;
