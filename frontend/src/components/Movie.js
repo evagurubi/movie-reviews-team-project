f13e953bc55ec3851b0d20403309464e1d121451
SpaceSC <ceciliasandor.22@gmail.com>
2021-07-21 21:41:12 +0200

    Save review to db

--- frontend/src/components/Movie.js
+++ frontend/src/components/Movie.js
@@ -2,6 +2,27 @@ import { useState } from "react";
 
 function Movie({ item }) {
   const [showWriteReview, setShowWriteReview] = useState(false);
+  const [newReview, setNewReview] = useState("");
+  const [review, setReview] = useState("");
+
+  const sendData = async (e) => {
+    e.preventDefault();
+
+    const response = await fetch("/api/review", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      // username = username: username
+      body: JSON.stringify({
+        title: item.title,
+        content: review,
+        movie_id: item.id,
+        reviewer: "Someone"
+      }),
+    });
+    const result = await response.json();
+    console.log(result);
+    
+  }
 
   return (
     <div>
@@ -10,8 +31,8 @@ function Movie({ item }) {
       <p>{item.overview}</p>
       <button onClick={() => setShowWriteReview(!showWriteReview)}>Leave Review</button>
       {showWriteReview && 
-      <form >
-        <input type="text" placeholder="Your review of the film" />
+      <form onSubmit={sendData}>
+        <input type="text" placeholder="Your review of the film" onChange={(e) => setReview(e.target.value)} />
         <input type="submit" value="SUBMIT"  />
       </form>}
     </div>
